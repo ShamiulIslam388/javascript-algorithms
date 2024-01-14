ad184d3356649688389f29c27cbf2aaee8835117
ShamiulIslam388 <shamiulislam388@gmail.com>
2024-01-14 19:07:22 +0600

    is valid parentheses

--- validParenthesis.js
+++ validParenthesis.js
@@ -1,24 +1,43 @@
-function isValid(s) {
-  let stack = [];
-  const pMap = {
-    "(": ")",
-    "{": "}",
-    "[": "]",
-  };
+// function isValid(s) {
+//   let stack = [];
+//   const pMap = {
+//     "(": ")",
+//     "{": "}",
+//     "[": "]",
+//   };
 
-  for (let char of s) {
-    if (pMap[char]) {
-      stack.push(char);
+//   for (let char of s) {
+//     if (pMap[char]) {
+//       stack.push(char);
+//     } else {
+//       const lastopenchar = stack.pop();
+//       if (char !== pMap[lastopenchar]) {
+//         return false;
+//       }
+//     }
+//   }
+
+//   return stack.length === 0;
+// }
+
+function isValid(s) {
+  if (s.length % 2 !== 0 || s.length === 0) return false;
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+      stack.push(s[i]);
     } else {
-      const lastopenchar = stack.pop();
-      if (char !== pMap[lastopenchar]) {
-        return false;
-      }
+      if (stack.length === 0) return false;
+
+      if (s[i] === ")" && stack.pop() !== "(") return false;
+      if (s[i] === "}" && stack.pop() !== "{") return false;
+      if (s[i] === "]" && stack.pop() !== "[") return false;
     }
   }
 
   return stack.length === 0;
 }
 
-const expression = "((()))";
+const expression = "()[]{}";
 console.log(isValid(expression));
